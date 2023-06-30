d99caacdd1e231933f3de7bc659518d628ff8827
Juan Perez <juan.perez@bigcommerce.com>
2023-06-30 17:49:07 -0600

    More descriptive values: master

--- fecha.py
+++ fecha.py
@@ -1,13 +1,13 @@
 #Módulo con funciones de fechas en español
 #Función que expresa la fecha en formato de carta
+from time import strftime
+import locale
+
 def fcarta():
-    from time import strftime
-    import locale
-    locale.setlocale(locale.LC_TIME, "es-MX") # Establece fecha local
+    loc = locale.getlocale()[0]
+    locale.setlocale(locale.LC_TIME, loc) # Establece fecha local
     return strftime('A %A, %d de %B de %Y:')
 #Crea un archivo nombrado con la fecha de hoy
 def farchivo():
-    from time import strftime
-    import locale
     archivo = open(strftime('%Y%m%d.txt'), 'w+')
     
